fca4b45a7348137e6677189f0e0dd86c2e47375d
Elvis8882 <148993408+Elvis8882@users.noreply.github.com>
2026-01-20 10:56:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.querySelectorAll("svg path").forEach(country => {
+  country.addEventListener("click", () => {
+    openPopup(country.id);
+  });
+});
+
+function openPopup(code) {
+  document.getElementById("country-name").innerText = code;
+  document.getElementById("country-content").innerText =
+    "Country profile will appear here.";
+  document.getElementById("popup").classList.remove("hidden");
+}
+
+function closePopup() {
+  document.getElementById("popup").classList.add("hidden");
+}
 
